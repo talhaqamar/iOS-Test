138fb522425c22f8c7f3d810cb849b4e8b9e17de
talhaqamar <talha.kicsit18@gmail.com>
2018-03-27 12:05:24 +1100

    API Call tested from code

--- iOS Test/ViewController.swift
+++ iOS Test/ViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import JASON
 
 class ViewController: UIViewController {
 
 override func viewDidLoad() {
 super.viewDidLoad()
-// Do any additional setup after loading the view, typically from a nib.
-}
 
+
+    }
 override func didReceiveMemoryWarning() {
 super.didReceiveMemoryWarning()
 // Dispose of any resources that can be recreated.
+}
+
+
+
+func loadData() -> String
+{
+let url = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+  
+ URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+
+        if let d = data {
+            if let value = String(data: d, encoding: String.Encoding.ascii) {
+
+                if let jsonData = value.data(using: String.Encoding.utf8) {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as! [String: Any]
+
+print(json["title"]!)
+                        if let arr = json["rows"] as? [[String: Any]] {
+                            debugPrint(arr)
+
+
+                        }
+
+                    } catch {
+                        NSLog("ERROR \(error.localizedDescription)")
+                    }
+                }
+            }
+
+        }
+        }.resume()
+        
+	return "";
 }
 
 
